c8e73b83a51ee42cbf9dd6492f6684eb6152a9f6
William Violette <violettewj@gmail.com>
2020-09-02 08:55:07 -0600

    update

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import {MainContainer} from '../components/MainContainer'
 
 
 const Index = (props) => {
-    console.log("propsINDX",{props})
     return(
         <>
         <Navbar />
@@ -18,17 +17,3 @@ const Index = (props) => {
 
 export default Index;
 
-export async function getStaticProps(context) {
-    const client = new WakaTimeClient(process.env.wakaTimeClient)
-    const myUserDetails = await client.getMe();
-     if(myUserDetails){
-         console.log("IMPORTANT", myUserDetails)
-         const stats = await client.getUserStats({
-             userId: myUserDetails.data.id,
-             range: RANGE.LAST_7_DAYS
-            });
-             return {
-               props: {stats}, // will be passed to the page component as props
-             }
-            }
-  }
