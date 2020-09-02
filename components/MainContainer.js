f4c97d59dbe725bf710ea228cfc95644f7bfb70d
William Violette <violettewj@gmail.com>
2020-09-02 10:44:40 -0600

    update

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -358,7 +358,7 @@ const MappedTime = styled.div`
   }
 `;
 export const MainContainer = (props) => {
-  const client = new WakaTimeClient(0b2b4c46-69d4-4edb-b6f8-f4995b8f9e96);
+  const client = new WakaTimeClient('0b2b4c46-69d4-4edb-b6f8-f4995b8f9e96');
   const [value, setValue] = useState(0);
   const [state, setState] = useState({
     totalTime: '',
