6f95b64882142890cea5a7ab20912f1c330f3b70
William Violette <violettewj@gmail.com>
2020-09-02 07:20:42 -0600

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,28 @@
 import App from 'next/app';
 import Theme from '../providers/ThemeProvider'
+import { WakaTimeClient, RANGE } from 'wakatime-client';
 import {DataProvider} from '../providers/DataProvider'
 
-export default function({Component, pageProps}){
+ function MyApp({Component, pageProps}){
     return (
-        // <DataProvider>
         <Theme>
             <Component {...pageProps}/>
         </Theme>
-        // </DataProvider>
+        
     )
 }
+// const client = new WakaTimeClient(process.env.wakaTimeClient)
+// MyApp.getInitialProps = async (appContext) => {
+//     const myUserDetails = await client.getMe();
+//      if(myUserDetails){
+//          console.log("IMPORTANT", myUserDetails)
+//          const stats = await client.getUserStats({
+//              userId: myUserDetails.data.id,
+//              range: RANGE.LAST_7_DAYS
+//            });
+//            console.log(stats)
+//            return stats
+//      }
+//      }
+
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -3,32 +3,16 @@ import styled from 'styled-components'
 import {Navbar} from '../components/Navbar'
 import {HeaderSection} from '../components/HeaderSection'
 import {MainContainer} from '../components/MainContainer'
-// import { WakaTimeClient, RANGE } from 'wakatime-client';
-// const client = new WakaTimeClient(process.env.wakaTimeClient)
 
 
-const Index = ({stats}) => {
+const Index = (props) => {
     return(
         <>
         <Navbar />
         <HeaderSection />
-        <MainContainer stats={stats}/>
+        <MainContainer />
         </>
     )
 }
-
-// Index.getInitialProps = async (ctx) => {
-//     const myUserDetails = await client.getMe();
-//     if(myUserDetails){
-//         console.log("IMPORTANT", myUserDetails)
-//         const stats = await client.getUserStats({
-//             userId: myUserDetails.data.id,
-//             range: RANGE.LAST_7_DAYS
-//           });
-//           console.log(stats)
-//           return stats
-//     }
-
-// }
 
 export default Index;
